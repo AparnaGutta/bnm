6c8ae0e9561ab1a808b7aff67056390a9762b010
AparnaGutta <anu.aparna419@gmail.com>
2018-07-13 21:49:40 +0530

    second

---
+++ bin/sample/Class.java
@@ -0,0 +1,22 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Class 
+{
+
+	@Test
+	public void m1()
+	{
+		System.out.println("HI");
+	}
+	
+	@Test
+	public void m2()
+	{
+		System.out.println("hello");
+	}
+	
+	
+	
+}
